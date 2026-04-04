7500489590df801f0548107170dd80bfb918002f
Thomas Dombrowski <thomasdombrowski@Mac-116.lan>
2026-04-03 20:44:59 -0500

    Standardize page file weights to lowercase to resolve case-sensitivity issues on Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import Home from "./pages/Home";
-import ServiceDetail from "./pages/ServiceDetail";
-import LocationDetail from "./pages/LocationDetail";
+import Home from "./pages/home";
+import ServiceDetail from "./pages/serviceDetail";
+import LocationDetail from "./pages/locationDetail";
 import ScrollToTop from "./components/layout/ScrollToTop";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contact";
 
 import PrivacyPolicy from "./app/privacy-policy/page";
 import TermsAndConditions from "./app/terms-and-conditions/page";
